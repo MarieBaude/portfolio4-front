d6bf0c11e629fabd41ea204348f0b17ca5750b9f
Marie Baude <mariebaude@maries-mbp.home>
2024-12-14 13:26:58 +0100

    init project

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "@solidjs/meta";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Home</Title>
+      <h1>Home</h1> 
+    </main>
+  );
+}
