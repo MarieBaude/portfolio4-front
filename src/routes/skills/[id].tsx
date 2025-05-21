76088b83ec0d4e78573df109931cd66e236eb140
Marie Baude <mariebaude@Maries-MacBook-Pro.local>
2025-05-21 14:11:39 +0200

    id

--- src/routes/skills/[id].tsx
+++ src/routes/skills/[id].tsx
@@ -1,16 +1,21 @@
 import { Title } from "@solidjs/meta";
-import { useLocation } from "@solidjs/router";
-import { Show } from "solid-js";
-import type { Skill } from "../../data/skills/skills.types";
-import { SKILLS_DATA } from "../../data/skills/skills.data";
+
+import { SKILLS_DATA } from "~/data/skills/skills.data";
 
 export default function SkillCard() {
 
   return (
     <main>
-      <Title>Skill Details</Title>
-      <h1>Skill Details</h1>
-     
+      <Title>{SKILLS_DATA[0].title}</Title> 
+      <h1>{SKILLS_DATA[0].title}</h1> 
+
+       {/* Définition  */}
+
+       {/* Preuves */}
+
+       {/* Auto citique */}
+
+       {/* Evolution */}
     </main>
   );
 }
