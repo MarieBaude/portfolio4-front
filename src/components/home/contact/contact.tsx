f0a1568a2c4e5ae28efc2eed33313f20d0c23ce9
Marie Baude <baudemarie95@gmail.com>
2025-06-26 21:39:31 +0200

    add netlify in contact form

--- src/components/home/contact/contact.tsx
+++ src/components/home/contact/contact.tsx
@@ -5,14 +5,17 @@ export default function Contact() {
     <section class="contact_content">
       <div>
         <h2>Contact</h2>
-        <form>
+        <form name="contact" netlify>
           <input type="text" placeholder="Nom" required />
           <input type="email" placeholder="Email" required />
           <textarea placeholder="Message" required></textarea>
           <button type="submit">Envoyer</button>
         </form>
       </div>
-      <img src="public/images/home/Rectangle_12.png" alt="illustration formulaire contact" />
+      <img
+        src="public/images/home/Rectangle_12.png"
+        alt="illustration formulaire contact"
+      />
     </section>
   );
 }
